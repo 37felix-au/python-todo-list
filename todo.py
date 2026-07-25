0b555667b29cfd3ca8a93a6addeb7d66a730fa24
backend-dev-20 <37@felix-au.me>
2026-07-25 15:46:04 +0530

    Implement features in todo.py - Version 4

--- todo.py
+++ todo.py
@@ -4,3 +4,10 @@ tasks = []
 
 def add_task(task):
     tasks.append(task)
+    print(f"Added task: {task}")
+
+def list_tasks():
+    if not tasks:
+        print("No tasks found.")
+    for idx, task in enumerate(tasks, 1):
+        print(f"{idx}. {task}")
